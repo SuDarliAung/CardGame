cc6799eb7071248a2949a832b59595eaf14b11d8
SuDarliAung <109328953+SuDarliAung@users.noreply.github.com>
2022-12-23 17:54:27 +0630

    Update Main.java

--- CardGame/src/main/java/com/cardgame/data/Main.java
+++ CardGame/src/main/java/com/cardgame/data/Main.java
@@ -8,7 +8,6 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         CardData[] game = new CardData[52];
 
         genCardDeck(game);
